c3b8e15cf3a3ce1051bb9cd19900a7c115808377
kira <myemail>
2020-05-26 03:01:07 +0800

    实现对数据库的删改查，还有模板语言的for以及格式

--- App2/url.py
+++ App2/url.py
@@ -5,4 +5,7 @@ from App2 import views
 urlpatterns = [
     re_path('addstudent/', views.addstudent),
     re_path('kira/', views.response),
+    re_path('updatestudent', views.update_student),
+    re_path('checkstudent', views.check_student),
+    re_path('delestudent', views.dele_student)
 ]
--- App2/views.py
+++ App2/views.py
@@ -15,3 +15,22 @@ def addstudent(request):
     student.age = 10
     student.save()
     return HttpResponse('成功调用')
+
+
+def update_student(request):
+    obj = Student.objects.get(name='Kira')
+    obj.name = 'Tom'
+    obj.save()
+    return HttpResponse('成功修改')
+
+
+def check_student(request):
+    obj = Student.objects.all()
+    return render(request, 'checkstudent.html', {'obj': obj})
+
+
+def dele_student(request):
+    obj = Student.objects.get(name='tom')
+    obj.delete()
+    obj.save()
+    return HttpResponse('删除成功')
