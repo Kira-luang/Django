d78dba6a8d29609b11a59944bfeb600cfa4c2e21
kira <myemail>
2020-05-29 02:06:27 +0800

    Django:过滤器以及使用示范

--- Django基础练习.py
+++ Django基础练习.py
@@ -233,6 +233,21 @@ forloop.last如果当前循环是最后一个就是True
 {% widthratio xx 1 3 %} -> 1是分母,3是分子
 '''
 
+# 9.过滤器
+'''
+格式: {{var | 过滤器}}
+{% x | divisible by 2 %}->"|"表示把x代入divisible by 2中
+divisible by 2->能够被2整除,配合if使用(过滤器)
+
+加法:{{var |add:6}} -> 加6
+减法:{{var |add:-6}} -> 减6
+大写:{{var |upper}} -> 了解即可
+小写:{{var |lower}} -> 了解即可
+默认值:{{var |default value}} -> 了解即可
+函数传参:{{var |join"参数"}} -> 了解即可
+日期:{{var |data:'y-m-d'}} -> 了解即可
+'''
+
 # Django流程:
 '''
 1.创建项目: django-admin startproject mysite
